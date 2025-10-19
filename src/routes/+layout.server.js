721a6b91f2db9db60cb6f70c3a3ed40a14648154
Kai Maurin-Jones <kmaurinjones@gmail.com>
2025-10-19 14:35:45 -0400

    Fix layout package path for build

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,13 +1,7 @@
 import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { resolve } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const pkg = JSON.parse(
-  readFileSync(join(__dirname, '../../package.json'), 'utf-8')
-);
+const pkg = JSON.parse(readFileSync(resolve(process.cwd(), 'package.json'), 'utf-8'));
 
 export function load() {
   return {
