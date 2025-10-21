8858217847f6e8a0909704c1c4f3b3cd6b68195f
kmaurinjones <kmaurinjones@gmail.com>
2025-10-20 21:05:33 -0400

    Remove Technical and Projects from navigation (temporarily)

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,9 +3,7 @@
 
   const links = [
     { href: '/', label: 'About' },
-    { href: '/technical', label: 'Technical' },
     { href: '/experience', label: 'Experience' },
-    { href: '/projects', label: 'Projects' },
     { href: '/thoughts', label: 'Thoughts' },
     { href: '/contact', label: 'Contact' },
   ];
